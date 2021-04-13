55493682ce7e554162f902f3dfd045ef157531df
George A. Michel <george@nomadscience.org>
2021-04-13 15:43:00 -0700

    Add hello world structure
    Just some simple imports to show everything is working as expected.

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './main.css';
+import foo from './lib';
+
+foo();
---
+++ src/lib/index.js
@@ -0,0 +1,4 @@
+console.log('Imported lib/index.js');
+export default function foo() {
+  console.log('Hello foo from lib.');
+}
